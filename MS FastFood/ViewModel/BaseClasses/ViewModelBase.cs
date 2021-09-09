a890ef01d560c5f96e95a48f9315638dd6c224ff
ZStachura <zmstachura@gmail.com>
2021-09-09 16:05:25 +0200

    Listboxes( in progress )

---
+++ MS FastFood/ViewModel/BaseClasses/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_FastFood.ViewModel.BaseClasses
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(params string[] namesOfProperties)
+        {
+            if (PropertyChanged != null)
+            {
+                
+                foreach (var prop in namesOfProperties)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                }
+            }
+        }
+    }
+}
---
+++ MS FastFood/ViewModel/DBList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_FastFood.ViewModel
+{
+    using Model;
+    using Model.Encje;
+    using BaseClasses;
+    using System.Windows.Input;
+
+    class DBList : ViewModelBase
+    {
+        private Model model = null;
+        private ObservableCollection<burgers> Burgers = null;
+
+
+        public DBList(Model model)
+        {
+            this.model = model;
+            Burgers = model.Burgers;
+        }
+
+        public burgers CurrentBurger { get; set; }
+
+        public ObservableCollection<burgers> burgers
+        {
+            get { return Burgers; }
+            set
+            {
+                Burgers = value;
+                onPropertyChanged(nameof(burgers));
+            }
+        }
+
+        public void RefreshBurgers() => Burgers = model.Burgers;
+
+        private ICommand Zburgers = null;
+        public ICommand ZBurgers
+        {
+            get
+            {
+                if (Zburgers == null)
+                    Zburgers = new RelayCommand(
+                        arg =>
+                        {
+                            Burgers = model.Burgers;
+
+                        }
+                        ,
+                        arg => true);
+                return Zburgers;
+            }
+        }
+    }
+}
